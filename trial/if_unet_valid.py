78dee1ef4df996d6b45e8a7a5b1248da96297488
zhuboyuan <boyuanzhu03@outlook.com>
2025-09-08 21:38:27 +0800

    remove following trial folders

--- trial/if_unet_valid.py
+++
@@ -1,8 +0,0 @@
-# 快速验证脚本
-from diffusers import UNet2DConditionModel
-
-# 确保加载的是 SD 1.5 的 UNet
-unet = UNet2DConditionModel.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="unet")
-
-print(f"Number of up_blocks: {len(unet.up_blocks)}") # 会打印 4
-print(unet.up_blocks[3]) # 不会报错
